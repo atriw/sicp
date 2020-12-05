20bd7084e64a90aea85c317df9b68af316d1aa8a
atriw <875241499@qq.com>
2020-12-05 20:59:19 +0800

    Refine 2.30

--- 2/2.30.scm
+++ 2/2.30.scm
@@ -5,6 +5,9 @@
                     (square-tree (cdr tree))))))
 
 (define (square-tree-map tree)
-  (cond ((null? tree) '())
-        ((not (pair? tree)) (square tree))
-        (else (map square-tree-map tree))))
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+           (square-tree-map sub-tree)
+           (square sub-tree)))
+       tree))
+
