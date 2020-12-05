3d0fbe338a7884cb0df09471bed87271303a7e93
atriw <875241499@qq.com>
2020-12-06 00:23:09 +0800

    Exercise: 2.40

---
+++ 2/2.40.scm
@@ -0,0 +1,8 @@
+(load "../list.scm")
+
+(define (unique-pairs n)
+  (flatmap
+    (lambda (i)
+      (map (lambda (j) (list i j))
+           (enumerate-interval 1 (- i 1))))
+    (enumerate-interval 1 n)))
---
+++ 2/2.40_test.scm
@@ -0,0 +1,7 @@
+(load "../test.scm")
+(load "2.40.scm")
+
+(define (test)
+  (let ((want (list (list 2 1) (list 3 1) (list 3 2) (list 4 1) (list 4 2) (list 4 3))))
+    (assert-eq want (unique-pairs 4) "Failed unique-pairs")))
+(test)
