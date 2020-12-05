815c90c60a553584780616c7ba9b7c912c7dd0b4
atriw <875241499@qq.com>
2020-12-05 15:18:57 +0800

    Exercise: 2.19

---
+++ 2/2.19_test.scm
@@ -0,0 +1,7 @@
+(load "../test.scm")
+(load "2.19.scm")
+
+(define (test)
+  (begin (assert-eq 292 (cc 100 us-coins) "Failed")
+         (assert-eq 292 (cc 100 (reverse us-coins)) "Failed reverse")))
+(test)
