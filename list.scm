68f9576b3448ca42e1b51893c1e5bb5c9f76243f
atriw <875241499@qq.com>
2020-12-06 00:13:12 +0800

    Add enumerate-interval to list.scm

--- list.scm
+++ list.scm
@@ -6,3 +6,8 @@
 
 (define (flatmap proc sequence)
   (accumulate append '() (map proc sequence)))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+    '()
+    (cons low (enumerate-interval (+ 1 low) high))))
