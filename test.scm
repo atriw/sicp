1fa656b2e734faa5d9a68ec08369a6d2c4accd5a
atriw <875241499@qq.com>
2020-12-05 14:30:28 +0800

    Refine test.scm: equal

--- test.scm
+++ test.scm
@@ -1,7 +1,11 @@
 (define (equal p1 p2)
-  (cond ((and (null? p1) (null? p2)) #t)
+  (cond
+        ((and (null? p1) (null? p2)) #t)
         ((and (null? p1) (not (null? p2))) #f)
         ((and (not (null? p1)) (null? p2)) #f)
+        ((and (not (pair? p1)) (not (pair? p2))) (= p1 p2))
+        ((and (pair? p1) (not (pair? p2))) #f)
+        ((and (not (pair? p1)) (pair? p2)) #f)
         (else (and (= (car p1) (car p2)) (equal (cdr p1) (cdr p2))))))
 
 (define (assert op want got msg)
