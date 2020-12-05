eb0d08b086cc8d04c3ad2ef2089462509b65da8d
atriw <875241499@qq.com>
2020-12-05 19:20:50 +0800

    Refine test.scm: better error format

--- test.scm
+++ test.scm
@@ -1,7 +1,11 @@
 (define (assert op want got msg)
   (if (op want got)
     (begin (display "Test pass.") (newline))
-    (error msg)))
+    (error msg
+           (error-irritant/noise ". want: ")
+           want
+           (error-irritant/noise ", got: ")
+           got)))
 
 (define (assert-eq want got msg)
   (assert equal? want got msg))
