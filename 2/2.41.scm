6cba202b92f35f9a306dfbae9c8fb4697be538fc
atriw <875241499@qq.com>
2020-12-06 02:35:11 +0800

    Exercise: 2.41

---
+++ 2/2.41.scm
@@ -0,0 +1,10 @@
+(load "2.17.scm")
+(load "2.40.scm")
+
+(define (three-sum n s)
+  (map (lambda (pair)
+         (append pair (list (fold-left - s pair))))
+       (filter (lambda (pair)
+                 (let ((last (fold-left - s pair)))
+                   (and (> last 0) (< last (car (last-pair pair))))))
+               (unique-pairs n))))
