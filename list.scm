ffcd33bc782cd477a0bbe16420c02ca02ee5c106
atriw <875241499@qq.com>
2020-12-05 21:48:11 +0800

    Add accumulate to list.scm

--- list.scm
+++ list.scm
@@ -1,4 +1,6 @@
-(define (append list1 list2)
-  (if (null? list1)
-    list2
-    (cons (car list1) (append (cdr list1) list2))))
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+    initial
+    (op (car sequence)
+        (accumulate op initial (cdr sequence)))))
+
