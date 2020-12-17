9e3984053b273e86820bf8d7fb506dc3c19ecb50
atriw <875241499@qq.com>
2020-12-18 01:44:44 +0800

    Exercise: 2.69

---
+++ 2/2.69.scm
@@ -0,0 +1,15 @@
+(load "../huffman.scm")
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge set)
+  (if (= (length set) 1)
+    (car set)
+    (let ((first (car set))
+          (second (cadr set))
+          (remaining (cddr set)))
+      (successive-merge
+        (adjoin-set
+          (make-code-tree first second)
+          remaining)))))
---
+++ 2/2.69_test.scm
@@ -0,0 +1,12 @@
+(load "../test.scm")
+(load "2.69.scm")
+(load "2.68.scm")
+
+(define sample-tree (generate-huffman-tree '((A 4) (B 2) (C 1) (D 1))))
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+(define sample-symbols '(A D A B B C A))
+
+(define (test)
+  (begin (assert-eq sample-message (encode sample-symbols sample-tree) "Failed encode")
+         (assert-eq sample-symbols (decode sample-message sample-tree) "Failed decode")))
+(test)
