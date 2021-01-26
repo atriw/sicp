89d9f0232e1ae687971503fd65893ef2a9260f58
atriw <875241499@qq.com>
2021-01-26 20:16:14 +0800

    Exercise: 3.3

---
+++ 3/3.3_test.scm
@@ -0,0 +1,9 @@
+(load "../test.scm")
+(load "3.3.scm")
+
+(define (test)
+  (let ((acc (make-account 100 'some-password)))
+    (begin (assert-eq 70 ((acc 'some-password 'withdraw) 30) "Failed withdraw")
+           (assert-eq "Insufficient funds" ((acc 'some-password 'withdraw) 80) "Failed withdraw")
+           (assert-eq "Incorrect password" ((acc 'incorrect-password 'withdraw) 30) "Failed incorrect password"))))
+(test)
