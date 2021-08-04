84f0755cddbbdb52c5a41fba02d38dd81b6c8884
atriw <875241499@qq.com>
2021-08-04 23:07:03 +0800

    Exercise: 3.7

---
+++ 3/3.7_test.scm
@@ -0,0 +1,24 @@
+(load "../test.scm")
+(load "./3.7.scm")
+
+(define (test)
+  (define peter-acc (make-account 100 'open-sesame))
+  (define paul-acc (make-joint peter-acc 'open-sesame 'rosebud))
+  (define wrong-acc (make-joint peter-acc 'wrong 'sun))
+  (begin (assert-eq
+           70
+           ((peter-acc 'open-sesame 'withdraw) 30)
+           "Failed withdraw peter")
+         (assert-eq
+           50
+           ((paul-acc 'rosebud 'withdraw) 20)
+           "Failed withdraw paul")
+         (assert-eq
+            80
+            ((peter-acc 'open-sesame 'deposit) 30)
+            "Failed deposit peter")
+         (assert-eq
+           "Incorrect password"
+           ((wrong-acc 'sun 'withdraw) 30)
+           "Failed withdraw wrong-acc")))
+(test)
