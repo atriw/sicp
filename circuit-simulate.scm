e9a73c4c0d123af9b64f1ab3349a028fad18ffe8
atriw <875241499@qq.com>
2021-08-08 06:58:39 +0800

    Add full-adder and half-adder

--- circuit-simulate.scm
+++ circuit-simulate.scm
@@ -7,6 +7,21 @@
 (define and-gate-delay 3)
 (define or-gate-delay 5)
 
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire)) (c1 (make-wire)) (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
 (define (inverter input output)
   (define (invert-input)
     (let ((new-value (logical-not (get-signal input))))
