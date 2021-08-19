151fb8cd7ce9a924c69158ee4bdec03f6f054c44
atriw <875241499@qq.com>
2021-08-20 01:27:12 +0800

    Refactor evaluator tests

--- 4/4.1_test.scm
+++ 4/4.1_test.scm
@@ -1,20 +1,20 @@
 (load "test.scm")
 (load "4/4.1.scm")
-(load "evaluator.scm")
-(load "environment.scm")
-(load "syntax.scm")
+(load "evaluator_test.scm")
 
 (define probe-procedure
   '(define (probe) (define s 1) (+ (begin (set! s (+ s 1)) s)
                                    (begin (set! s (* s 2)) s))))
 (define (test make-list-of-values want msg)
-  (let ((syntax (make-syntax))
-        (env-model (make-environment-model)))
-    (let ((evaluator (make-evaluator syntax env-model))
-          (the-global-environment ((env-model 'setup-environment))))
-      (let ((list-of-values (make-list-of-values evaluator syntax)))
-        ((evaluator 'mock-list-of-values) list-of-values)
-        ((evaluator 'eval) probe-procedure the-global-environment)
-        (assert-eq want ((evaluator 'eval) '(probe) the-global-environment) msg)))))
+  (define test-suite
+    (setup-test
+      '()
+      (lambda (evaluator syntax)
+        ((evaluator 'mock-list-of-values) (make-list-of-values evaluator syntax)))))
+  (define (test-fn eval env)
+    (eval probe-procedure env)
+    (assert-eq want (eval '(probe) env) msg))
+  (test-suite test-fn))
+
 (test make-list-of-values-left-to-right 6 "Failed list-of-values left to right")
 (test make-list-of-values-right-to-left 5 "Failed list-of-values right to left")
--- evaluator_test.scm
+++ evaluator_test.scm
@@ -8,3 +8,19 @@
     (let ((evaluator (make-evaluator syntax env-model)))
       (let ((loop (make-driver-loop evaluator env-model)))
         (loop)))))
+
+(define (setup-test
+          modify-syntax ; lambda (syntax) -> syntax
+          mock) ; lambda (evaluator syntax) -> ()
+  (define syntax
+    (if (not (null? modify-syntax))
+      (modify-syntax (make-syntax))
+      (make-syntax)))
+  (define env-model (make-environment-model))
+  (define env ((env-model 'setup-environment)))
+  (define evaluator (make-evaluator syntax env-model))
+  (define eval (evaluator 'eval))
+  (define (test fn) ; fn: lambda (eval env) -> ()
+    (fn eval env))
+  (if (not (null? mock)) (mock evaluator syntax))
+  test)
