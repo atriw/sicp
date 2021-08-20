16cf37f72176357fcf6bfbea4e1e683bea4307f8
atriw <875241499@qq.com>
2021-08-21 05:30:47 +0800

    Exercise: 4.8

---
+++ 4/4.8_test.scm
@@ -0,0 +1,19 @@
+(load "test.scm")
+(load "4/4.8.scm")
+(load "evaluator_test.scm")
+
+(define (test)
+  (define test-suite
+    (setup-test new-syntax '()))
+  (define (test-fn eval env)
+    (eval '(define (fib n)
+             (let fib-iter ((a 1)
+                            (b 0)
+                            (count n))
+               (if (= count 0)
+                 b
+                 (fib-iter (+ a b) a (- count 1)))))
+          env)
+    (assert-eq 13 (eval '(fib 7) env) "Failed named let"))
+  (test-suite test-fn))
+(test)
