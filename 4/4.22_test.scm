d3ab326c515ec1789243afb2d8cde075a8992829
atriw <875241499@qq.com>
2021-08-23 21:07:35 +0800

    Exercise: 4.22

---
+++ 4/4.22_test.scm
@@ -0,0 +1,13 @@
+(load "test.scm")
+(load "4/4.22.scm")
+(load "evaluator_test.scm")
+
+(define (test)
+  (define test-suite
+    (setup-integral-test new-syntax '() '()))
+  (define (test-fn eval env)
+    (assert-eq 7
+               (eval '(let ((x 3) (y 4)) false (+ x y)) env)
+               "Failed let->combination"))
+  (test-suite test-fn))
+(test)
